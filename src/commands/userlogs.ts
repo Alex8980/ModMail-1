c44773f0d7da73ebe706ad82ce8cebe299256807
Alex8980 <92227984+Alex8980@users.noreply.github.com>
2021-10-09 15:01:17 -0500

    Initialize rewrite

---
+++ src/commands/userlogs.ts
@@ -0,0 +1,16 @@
+import Command from '../lib/structures/Command';
+
+export default new Command('userlogs', async (caller, cmd) => {
+	if (!process.env.LOGS_URL)
+		return caller.utils.discord.createMessage(cmd.channel.id, 'You have no logs URL configured.');
+	if (!cmd.args[0])
+		return caller.utils.discord.createMessage(cmd.channel.id, 'Provide a user ID.');
+	const links = await caller.db.getUserLogs(cmd.args[0]);
+	if (!links) return caller.utils.discord.createMessage(cmd.channel.id, 'No logs found.');
+
+	caller.utils.discord.createMessage(cmd.channel.id, links.map((log) => `${process.env.LOGS_URL}log?id=${log._id}`).join('\n'));
+},
+{
+	level: 'SUPPORT',
+	aliases: ['logs']
+});
